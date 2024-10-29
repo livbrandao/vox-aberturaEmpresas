1f34ceb047810bc9671c675cd76fb070d44747de
Livia Brandao <liviatoledo27@gmail.com>
2024-10-28 23:35:56 -0300

    displays cards with existing companies on the homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,28 @@ import { BrowserModule } from "@angular/platform-browser";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { NavbarComponent } from "./components/navbar/navbar.component";
+import { NavComponent } from "./components/nav/nav.component";
 
 import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatSidenavModule } from "@angular/material/sidenav";
 import { MatIconModule } from "@angular/material/icon";
 import { MatCardModule } from "@angular/material/card";
 import { MatDialogModule } from "@angular/material/dialog";
 import { EmpresaListaComponent } from "./pages/empresa-lista/empresa-lista.component";
 import { CardComponent } from "./shared/card/card.component";
-import { DetalhesComponent } from "./modals/detalhes/detalhes.component";
+import { DetailCardComponent } from "./shared/detail-card/detail-card.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { NovaEmpresaComponent } from './pages/nova-empresa/nova-empresa.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
+    NavComponent,
     EmpresaListaComponent,
     CardComponent,
-    DetalhesComponent,
+    DetailCardComponent,
+    HeaderComponent,
+    NovaEmpresaComponent,
   ],
   imports: [
     BrowserModule,
@@ -30,6 +35,7 @@ import { DetalhesComponent } from "./modals/detalhes/detalhes.component";
     MatIconModule,
     MatCardModule,
     MatDialogModule,
+    MatSidenavModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/pages/empresa-lista/empresa-lista.component.ts
+++ src/app/pages/empresa-lista/empresa-lista.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-empresa-lista',
-  templateUrl: './empresa-lista.component.html',
-  styleUrls: ['./empresa-lista.component.css']
+  selector: "app-empresa-lista",
+  templateUrl: "./empresa-lista.component.html",
+  styleUrls: ["./empresa-lista.component.css"],
 })
 export class EmpresaListaComponent {
+  selectedDetail: { title: string; subtitle: string } | null = null;
 
+  onViewDetails(detail: { title: string; subtitle: string }) {
+    this.selectedDetail = detail;
+  }
 }
