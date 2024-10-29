630fe3864bafa16255f989c17dfd984a4cad6726
Livia Brandao <liviatoledo27@gmail.com>
2024-10-29 00:50:45 -0300

    returns the data on the cards and in the details view

--- src/app/pages/empresa/empresa-lista/empresa-lista.component.ts
+++ src/app/pages/empresa/empresa-lista/empresa-lista.component.ts
@@ -1,4 +1,6 @@
 import { Component } from "@angular/core";
+import { Empresa } from "../empresa.model";
+import { EmpresaListaService } from "./empresa-lista.service";
 
 @Component({
   selector: "app-empresa-lista",
@@ -6,7 +8,16 @@ import { Component } from "@angular/core";
   styleUrls: ["./empresa-lista.component.css"],
 })
 export class EmpresaListaComponent {
+  empresaData: Empresa[] = [];
   selectedDetail: { title: string; subtitle: string } | null = null;
+
+  constructor(private mockDataService: EmpresaListaService) {}
+
+  ngOnInit(): void {
+    this.mockDataService.getEmpresasData().subscribe((data) => {
+      this.empresaData = data;
+    });
+  }
 
   onViewDetails(detail: { title: string; subtitle: string }) {
     this.selectedDetail = detail;
---
+++ src/app/pages/empresa/empresa.model.ts
@@ -0,0 +1,33 @@
+export interface Empresa {
+  id?: number;
+  solicitante: {
+    ds_responsavel: string;
+    nu_cpf: string;
+    date_nascimento: string;
+  };
+  empresa: {
+    ds_nome_fantasia: string;
+    co_entidade_registro: number;
+    co_natureza_juridica: number;
+    endereco: {
+      co_cep: number;
+      ds_logradouro: string;
+      co_numero: string;
+      ds_complemento: string | null;
+      ds_bairro: string;
+      ds_municipio: string;
+      co_uf: string;
+    };
+  };
+}
+
+export interface EntidadeRegistro {
+  key: number;
+  value: string;
+  id?: string;
+}
+
+export interface Dados {
+  empresas: Empresa[];
+  entidadeRegistro: EntidadeRegistro[];
+}
